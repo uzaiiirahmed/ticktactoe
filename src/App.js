802cd891de95e1bb580f3ee0af32b9981e3d38d7
Uzair Ahmed <uzairahmedshiekh2003@gmail.com>
2025-10-24 16:46:31 +0500

    Refactor App component to implement Tic Tac Toe game logic and add styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react' ; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function Square({value , onSquareClick}) 
+  {
+
+    return( <button className="square" onClick = {onSquareClick}> {value }</button> );
+
+  }
+
+export default function Board()
+{
+  const [squares , setSquares] = useState(Array(9).fill(null));
+  
+  function handleClick() {
+    const nextSquares = squares.slice() ;
+    nextSquares[0] = 'X' ;
+    setSquares(nextSquares) ;
+  }
+  
+  return(
+  <>
+  <div className="board-row">
+  <Square value = {squares[0]} onSquareClick={handleClick} />
+  <Square value = {squares[1]} onSquareClick={handleClick} />
+  <Square value = {squares[2]} onSquareClick={handleClick} />
+  </div>
+
+  <div className="board-row">
+  <Square value = {squares[3] } onSquareClick={handleClick} />
+  <Square value = {squares[4] } onSquareClick={handleClick}/>
+  <Square value = {squares[5] } onSquareClick={handleClick}/>
+  </div>
+
+  <div className="board-row">
+  <Square value = {squares[6] } onSquareClick={handleClick} />
+  <Square value = {squares[7] } onSquareClick={handleClick}/>
+  <Square value = {squares[8] } onSquareClick={handleClick}/>
+  </div>
+  </>
   );
 }
 
-export default App;
+
+
