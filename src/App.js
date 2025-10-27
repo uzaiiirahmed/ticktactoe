4211b851e47eedba068b63edb0ae17a2c6026981
Uzair Ahmed <uzairahmedshiekh2003@gmail.com>
2025-10-27 15:47:17 +0500

    chore : code snippet relocated

--- src/App.js
+++ src/App.js
@@ -61,6 +61,7 @@ export default function Game (){
   const [xIsNext, setXIsNext] = useState(true) ;
   const [history,setHistory] = useState ([Array(9).fill(null)]) ;
   const [currentMove , setCurrentMove] = useState ( 0) ;
+
   const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares){
@@ -73,8 +74,7 @@ export default function Game (){
 
   function jumpTo(nextMove){
     setCurrentMove(nextMove) ;
-    setXIsNext(nextMove % 2 === 0) ;
-    
+      setXIsNext(nextMove % 2 === 0) ;
   }
 
   const moves = history.map((squares,move)=> {
